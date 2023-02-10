6304fdc203bedbb333e2931f8b8a8190c66bac01
Ayush Solanki <ayush.solanki1981@gmail.com>
2023-02-10 15:59:51 +0530

    day 2 - task - 1 completed

---
+++ src/app/component/day2/day2-task1/Product.ts
@@ -0,0 +1,7 @@
+export class Product {
+  orderId!: number;
+  productName!: string;
+  quantity!: number;
+  billAmount!: number;
+  transaction!: string;
+}
--- src/app/component/day2/day2-task1/day2-task1.component.ts
+++ src/app/component/day2/day2-task1/day2-task1.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Product } from './Product';
 
 @Component({
   selector: 'app-day2-task1',
   templateUrl: './day2-task1.component.html',
-  styleUrls: ['./day2-task1.component.scss']
+  styleUrls: ['./day2-task1.component.scss'],
 })
 export class Day2Task1Component {
-
+  products: Array<Product> = [
+    {
+      orderId: 2001,
+      productName: 'iphone 11 pro',
+      quantity: 1,
+      billAmount: 1850,
+      transaction: 'completed',
+    },
+    {
+      orderId: 2002,
+      productName: 'iphone 10',
+      quantity: 1,
+      billAmount: 990,
+      transaction: 'pending',
+    },
+    {
+      orderId: 2003,
+      productName: 'Samsung Galaxy S10',
+      quantity: 2,
+      billAmount: 1980,
+      transaction: 'completed',
+    },
+    {
+      orderId: 2004,
+      productName: 'Motorolla Edge+',
+      quantity: 1,
+      billAmount: 980,
+      transaction: 'pending',
+    },
+  ];
 }
