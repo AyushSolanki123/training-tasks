2068214f4732053d38d67aef63b5c2bccea23ba7
Ayush Solanki <ayush.solanki1981@gmail.com>
2023-02-10 15:47:08 +0530

    day 1 page complete

--- src/app/component/day1/day1.component.ts
+++ src/app/component/day1/day1.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Task } from '../Task';
 
 @Component({
   selector: 'app-day1',
   templateUrl: './day1.component.html',
-  styleUrls: ['./day1.component.scss']
+  styleUrls: ['./day1.component.scss'],
 })
 export class Day1Component {
-
+  data: Task = {
+    name: 'Day 1',
+    route: '/day1',
+    tasks: [
+      { id: 1, task: 'Data Binding', route: '/day1/task1' },
+      { id: 2, task: 'Event Binding', route: '/day1/task2' },
+    ],
+  };
 }
