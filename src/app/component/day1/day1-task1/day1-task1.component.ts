b47f51c1f97f4f66c0048e5561f4062de54aead1
Ayush Solanki <ayush.solanki1981@gmail.com>
2023-02-15 14:40:39 +0530

    day1 tassk completed

--- src/app/component/day1/day1-task1/day1-task1.component.ts
+++ src/app/component/day1/day1-task1/day1-task1.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-day1-task1',
   templateUrl: './day1-task1.component.html',
-  styleUrls: ['./day1-task1.component.scss']
+  styleUrls: ['./day1-task1.component.scss'],
 })
 export class Day1Task1Component {
-
+  name!: string;
 }
--- src/app/component/day1/day1-task2/day1-task2.component.ts
+++ src/app/component/day1/day1-task2/day1-task2.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-day1-task2',
   templateUrl: './day1-task2.component.html',
-  styleUrls: ['./day1-task2.component.scss']
+  styleUrls: ['./day1-task2.component.scss'],
 })
 export class Day1Task2Component {
+  username!: string;
+  password!: string;
+  success!: boolean;
+  submit(): void {
+    if (this.username == 'innovapptive' && this.password == 'innovapptive') {
+      this.success = true;
+    } else {
+      this.success = false;
+    }
+  }
 
+  reset(): void {
+    this.username = '';
+    this.password = '';
+    this.success = false;
+  }
 }
