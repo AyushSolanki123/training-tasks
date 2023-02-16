28a3f0249a7cf13dee74220970de2e9a3d8f1c03
Ayush Solanki <ayush.solanki1981@gmail.com>
2023-02-16 10:38:32 +0530

    day3 task2 completed

--- src/app/component/day3/day3-task2/day3-task2.component.ts
+++ src/app/component/day3/day3-task2/day3-task2.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-day3-task2',
   templateUrl: './day3-task2.component.html',
-  styleUrls: ['./day3-task2.component.scss']
+  styleUrls: ['./day3-task2.component.scss'],
 })
 export class Day3Task2Component {
-
+  username!: string;
+  password!: string;
+  success!: boolean;
+  submit(): void {
+    if (this.username == 'admin' && this.password == 'admin123') {
+      this.success = true;
+    } else {
+      this.success = false;
+    }
+  }
 }
