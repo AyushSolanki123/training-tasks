d24d809c5383d50e4f1a58729170c34d4f0673de
Ayush Solanki <ayush.solanki1981@gmail.com>
2023-02-21 13:01:11 +0530

    day7 task1 completed

--- src/app/component/day7/day7-task1/day7-task1.component.ts
+++ src/app/component/day7/day7-task1/day7-task1.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { Observable, map, of, reduce } from 'rxjs';
+
+interface Product {
+  pName: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-day7-task1',
   templateUrl: './day7-task1.component.html',
-  styleUrls: ['./day7-task1.component.scss']
+  styleUrls: ['./day7-task1.component.scss'],
 })
 export class Day7Task1Component {
+  products: Observable<Product> = of<Product[]>(
+    { pName: 'televison', price: 17999 },
+    { pName: 'air conditioner', price: 32499 },
+    { pName: 'smartphone', price: 24299 },
+    { pName: 'washing machine', price: 37999 },
+    { pName: 'dish washer', price: 52499 }
+  );
+  totalPrice$!: Observable<number>;
+  productName$!: Observable<string>;
+  productNameList: string[] = [];
 
+  constructor() {
+    this.totalPrice$ = this.products.pipe(
+      reduce((acc, itemsdet) => acc + itemsdet.price, 0)
+    );
+
+    this.productName$ = this.products.pipe(map((value) => value.pName));
+
+    this.productName$.subscribe(
+      (name) => this.productNameList.push(name),
+      (error) => console.log(error),
+      () => console.log('Completed')
+    );
+  }
 }
